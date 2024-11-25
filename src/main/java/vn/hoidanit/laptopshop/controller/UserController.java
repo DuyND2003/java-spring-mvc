61b0a54fad5f96bf9b58f10a6480866f9174d407
DuyND2003 <duynguyenusth@gmail.com>
2024-11-25 19:14:19 +0700

    jstl trong view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.hoidanit.laptopshop.service.UserService;
@@ -16,8 +17,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage(){
+    public String getHomePage(Model model){
         String test = this.UserService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", " from controller with model");
         return "hello";
     }
     
