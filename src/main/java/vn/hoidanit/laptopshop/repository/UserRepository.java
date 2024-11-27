c3a8a064ffd464a4d85785e3e3fbf9c88e819829
DuyND2003 <duynguyenusth@gmail.com>
2024-11-27 14:59:09 +0700

    display users

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -12,5 +12,5 @@ import vn.hoidanit.laptopshop.domain.User;
 public interface UserRepository extends JpaRepository<User, Long>{
     User save(User eric);
     List<User> findOneByEmail(String email);
-    
+    List<User> findAll();
 }
