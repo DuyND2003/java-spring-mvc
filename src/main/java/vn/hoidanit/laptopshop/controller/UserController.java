1f9de446e286c02e961f1685f06966d3b07f1288
DuyND2003 <duynguyenusth@gmail.com>
2024-11-29 09:17:07 +0700

    dynamic update view user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
+
+
 
 @Controller
 public class UserController {
@@ -37,6 +41,15 @@ public class UserController {
         return "admin/user/table-user";
     }
 
+    @RequestMapping("admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id){
+        User user = this.userService.getUserById(id); 
+        model.addAttribute("user", user);
+        model.addAttribute("id", id);
+        return "admin/user/show";
+
+
+    }
     @RequestMapping("/admin/user/create") // GET 
     public String getCreateUserPage(Model model){
         model.addAttribute("newUser", new User());
@@ -47,6 +60,26 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit){
         this.userService.handleSaveUser(hoidanit);
+        return "redirect:/admin/user";
+    }
+
+    @RequestMapping("/admin/user/update/{id}")
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User currentUser = this.userService.getUserById(id);
+        model.addAttribute("newUser", currentUser);
+        return "admin/user/update"; 
+    }
+    
+    @PostMapping("/admin/user/update")
+    public String postUpdateUser(Model mode, @ModelAttribute("newUser") User hoidanit){        
+        User currentUser = this.userService.getUserById(hoidanit.getId());
+        if(currentUser != null){
+            currentUser.setAddress(hoidanit.getAddress());
+            currentUser.setFullName(hoidanit.getFullName());
+            currentUser.setPhone(hoidanit.getPhone());
+            
+            this.userService.handleSaveUser(currentUser);
+        }
         return "redirect:/admin/user";
     }
     
